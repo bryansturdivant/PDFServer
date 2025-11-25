72119f84b0c2fa69a8e3b91a27ed54bfbb2c2f6a
Bryan <bryan.sturdivant@maine.edu>
2025-11-25 14:04:49 +0000

    routing is working, readying pdfDiscovery module

--- nodejs/routes/auth.js
+++ nodejs/routes/auth.js
@@ -10,6 +10,16 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+router.get('/data-structures', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pdfs/data-structures.pdf'));
+});
+
+router.get('/operating-systems', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pdfs/operating-systems.pdf'));
+});
+router.get('/computer-networks', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pdfs/computer-networks.pdf'));
+});
 // 404 handler for unknown routes
 router.use((req, res) => {
     res.status(404).send('Page not found');
---
+++ nodejs/routes/pdfDiscovery.js
@@ -0,0 +1,2 @@
+//This will be my pdf discovery module - Not sure how this works yet 
+
